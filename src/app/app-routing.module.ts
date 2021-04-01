2f5fe21a87819725071ffecb04c42b9a6f54a750
Kherin <kherin.j.s.bundhoo@gmail.com>
2021-04-01 22:26:38 +0400

    feature: active routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PageNotFoundComponent } from './core/components/page-not-found/page-not
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
   { path: 'podcasts', loadChildren: () => import('./features/podcasts/podcasts.module').then(m => m.PodcastsModule) },
   { path: 'guests', loadChildren: () => import('./features/guests/guests.module').then(m => m.GuestsModule) },
   { path: 'calendar', loadChildren: () => import('./features/calendar/calendar.module').then(m => m.CalendarModule) },
