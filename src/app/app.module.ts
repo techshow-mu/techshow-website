96461421cf80c86d1384ed935765eb0c8c0ecb16
Kherin <kherin.j.s.bundhoo@gmail.com>
2021-04-02 19:24:58 +0400

    fix: removed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
 import { ButtonComponent } from './button/button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
-    FooterComponent,
     ButtonComponent,
 
   ],
